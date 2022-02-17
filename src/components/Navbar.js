c0e82938f2a061cd28516fa3612da2e02edada4d
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-02-16 17:20:48 -0800

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+//src/components/Navbar.js
+
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <header>
+            <div id="nav-links">
+                <a href="#home">Home</a>
+                <a href="#dice">Dice</a>
+                <a href="#number">Number</a>
+                <a href="#coins">Coins</a>
+                <a href="#words">Words</a>
+                <a href="#password">Passwords</a>
+            </div>
+        </header>
+    );
+}
+
