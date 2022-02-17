8851a67d7d5feb342284cbddcd744f041714fbc1
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-02-17 14:47:36 -0800

    Initial commit, home page

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+//src/components/Footer.js
+
+import React from "react";
+import "./styles/Footer.css";
+import githubIcon from "./images/github_logo.png";
+
+export default function Footer(){
+    return(
+        <div id="footer">
+            <h3>Copyright © 2022 Jonathan Chen</h3>
+            <a href="https:/jonhpc.github.io/rngee"><img src={githubIcon} alt="github"></img></a>
+        </div>
+    );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 //src/components/Navbar.js
 
 import React from "react";
+import "./styles/Navbar.css";
 
 export default function Navbar(){
     return(
         <header>
+            <a href="#home"><h3>rngee.io</h3></a>
             <div id="nav-links">
-                <a href="#home">Home</a>
                 <a href="#dice">Dice</a>
                 <a href="#number">Number</a>
                 <a href="#coins">Coins</a>
                 <a href="#words">Words</a>
                 <a href="#password">Passwords</a>
+                <a href="#colors">Colors</a>
             </div>
         </header>
     );
