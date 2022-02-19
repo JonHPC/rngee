ad1aa036e2546ff4dc29ddfc41d403abe31fd5c1
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-02-18 17:28:13 -0800

    Added coin component

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,12 +1,29 @@
 //./src/components/Coins.js
-import React from "react";
+import React, {useState} from "react";
 import "./styles/Coins.css";
+import coinWhite from "./images/coin_white.png";
+import coinBlack from "./images/coin_black.png";
 
 export default function Coins(){
+    const [coin, setCoin] = useState(coinWhite);
+    const [result, setResult] = useState("Flip a coin");
 
+    const flipCoin = () => {
+        let flip = (Math.floor(Math.random() * 2) == 0);
+
+        if(flip){
+            setResult("Heads");
+            setCoin(coinWhite);
+        } else{
+            setResult("Tails");
+            setCoin(coinBlack);
+        }
+    }
     return(
         <div className="coins">
-            <h1>Coins</h1>
+            <img src={coin}></img>
+            <h1>{result}</h1>
+            <button onClick={flipCoin} className="coins__btn">Flip Coin</button>
         </div>
     );
 }
